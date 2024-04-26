a0e4f0ca4719286138e599fcc68818675633d70e
sukanyaparashar <sukanyaparashar111@gmail.com>
2024-04-26 14:28:46 -0600

    Add initial examples

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "go-ethereum-tutorial/deploy"
+
+func main() {
+	// Storage contract
+	storageContractAddress := deploy.DeployStorageContract()
+	deploy.StoreValue(storageContractAddress, 45)
+	deploy.ReadValue(storageContractAddress)
+
+	// TestERC20 contract
+	testERC20ContractAddress := deploy.DeployTestERC20Contract()
+	deploy.TransferTokens(testERC20ContractAddress, 10)
+}
